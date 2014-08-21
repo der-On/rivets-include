3ea270f2431d5f383e4c11884e5b4d5ef7f10e19
Ondrej Brinkel <info@anzui.de>
2014-08-22 01:28:23 +0200

    initial commit

---
+++ rivets-include.js
@@ -0,0 +1,84 @@
+/* global define: true */
+(function(root, factory) {
+    'use strict';
+    if (typeof exports === 'object') {
+        // CommonJS
+        factory(require('rivets'), require('browser-request'));
+    } else if (typeof define === 'function' && define.amd) {
+        // AMD. Register as an anonymous module.
+        define(['rivets', 'browser-request'], factory);
+    } else {
+        // Browser globals
+        factory(root.rivets, root.request);
+    }
+})(this, function(rivets, request) {
+    var cache = {};
+
+    rivets.binders.include = {
+        bind: function(el) {
+            var self = this;
+            var childView;
+            var keypath = this.keypath;
+            var view = this.view;
+
+            this.clear = function() {
+                if (childView) {
+                    childView.unbind();
+                }
+
+                el.innerHTML = '';
+            };
+
+            this.load = function(path) {
+                switch(typeof(path)) {
+                    case 'function': path = path(); break;
+                    case 'undefined':
+                        if ((keypath.startsWith("'") && keypath.endsWith("'")) ||
+                            (keypath.startsWith('"') && keypath.endsWith('"'))) {
+                            path = keypath.substring(1, keypath.length - 1);
+                        }
+                        break;
+                }
+
+                if (!path) {
+                    this.clear();
+                    return;
+                }
+
+                if (cache[path]) {
+                    include(cache[path]);
+                    return;
+                }
+
+                request.get(path, onLoaded);
+
+                function onLoaded(err, response, body) {
+                    if (err) {
+                        self.clear();
+                        if (console) console.error(err);
+                        return;
+                    }
+
+                    include(body);
+                    cache[path] = body;
+                }
+
+                function include(html) {
+                    var child = document.createElement('div');
+                    child.className = 'rv-include';
+                    child.innerHTML = html;
+                    el.appendChild(child);
+                    childView = rivets.bind(child, view.models);
+                }
+            };
+
+            this.load();
+        },
+        unbind: function(el) {
+            this.clear();
+        },
+        routine: function(el, value) {
+            this.load(value);
+        }
+    };
+});
